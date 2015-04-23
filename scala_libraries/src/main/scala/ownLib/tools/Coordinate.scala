1c246be63adf36d4bd8f12ebbe0bdd27ebb9b854
Christophe Cluizel <christophe.cluizel@gmail.com>
2015-04-23 21:00:41 +0200

    git feat(tools): add the method "==" for the Coordinate class
    The method "==" allows to compare 2 coordinates from each other.

--- scala_libraries/src/main/scala/ownLib/tools/Coordinate.scala
+++ scala_libraries/src/main/scala/ownLib/tools/Coordinate.scala
@@ -1,5 +1,19 @@
 package ownLib.tools
 
+import scala.Boolean
+
 class Coordinate(var x: Int, var y: Int) {
+
+  /**
+   * @param that The coordinate to compare with.
+   * @return true if the 2 coordinates are equal
+   */
+  def ==(that: Coordinate): Boolean = {
+    this.x == that.x && this.y == that.y
+  }
+
+  /**
+   * Redefine the "toString" method for the Coordinate class
+   */
   override def toString: String = "(" + x + ", " + y + ")"
 }
