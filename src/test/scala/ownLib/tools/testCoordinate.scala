cb73b9232e1e3e79434ffc33ddba90bcd8318e50
Christophe Cluizel <christophe.cluizel@gmail.com>
2015-04-27 21:38:50 +0200

    test(tools): add test to compare 2 Coordinate objects
    Add test for the "==" method of Coordinate class.

--- src/test/scala/ownLib/tools/testCoordinate.scala
+++ src/test/scala/ownLib/tools/testCoordinate.scala
@@ -12,6 +12,12 @@ class testCoordinate extends FlatSpec with Matchers {
 
   "A Coordinate" should "have a toString method" in {
     val f = fixtureCoordinate
-    f.coordinate.toString() should be ("(20, 10)")
+    f.coordinate.toString() should be("(20, 10)")
+  }
+
+  it should "be compared with another Coordinate" in {
+    val f = fixtureCoordinate
+    f.coordinate == new Coordinate(20, 10) should be(true)
+    f.coordinate == new Coordinate(20, 20) should be(false)
   }
 }
